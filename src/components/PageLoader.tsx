a8568454aa3dd991c145c3fef172717971833f69
onebyte <fernandoanibalreyes@gmail.com>
2023-09-24 22:22:51 -0400

    improved PageLoader

--- src/components/PageLoader.tsx
+++ src/components/PageLoader.tsx
@@ -1,9 +1,10 @@
 export default function PageLoader() {
   return (
-    <div className="w-full relative h-full bg-black">
-      <span className="absolute top-1/2 right-1/2 text-white text-3xl">
-        Loading...
-      </span>
+    <div className="w-full relative bg-gray-950 h-dvh flex justify-center">
+      <img
+        src="/Poke_Ball_icon.svg"
+        className="animate-spin max-h-32 self-center"
+      ></img>
     </div>
   );
 }
